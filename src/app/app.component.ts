f4907bc7a8e7e16c463374bd02fe70ad30baad5a
Dhimanda <dhiman.sarker@selise.ch>
2022-01-16 15:58:09 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toDoList';
+  list:any[]=[];
+  addTask(item:string){
+    console.warn(item)
+    this.list.push({id:this.list.length,name:item});
+    console.warn(this.list)
+  }
+  removeTask(id:number){
+    this.list=this.list.filter(item=>item.id!==id)
+  }
 }
